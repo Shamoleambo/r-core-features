180cd02d62ce0eb6e32f38e9ff7bf9824f93a241
Tiago Gomes <tidgomes@gmail.com>
2023-05-24 10:41:17 -0300

    create PostsList to embrace Post and make Post compoennt a list element

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import Post from './components/Post'
+import PostsList from './components/PostsList'
 
 function App() {
   return (
     <main>
-      <Post author='Tiago Gomes' content='The Super Book' />
-      <Post author='Semog Ogait' content='The Lamest Book' />
+      <PostsList />
     </main>
   )
 }
---
+++ src/components/PostsList.jsx
@@ -0,0 +1,12 @@
+import Post from './Post'
+
+function PostsList() {
+  return (
+    <ul>
+      <Post author='Tiago Gomes' content='The Super Book' />
+      <Post author='Semog Ogait' content='The Lamest Book' />
+    </ul>
+  )
+}
+
+export default PostsList
