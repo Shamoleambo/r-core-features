a9057c7b3985ef0216ee25e2f906cf45a22412a8
Tiago Gomes <tidgomes@gmail.com>
2023-05-24 11:00:11 -0300

    add NewPost component to PostsList

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
+import NewPost from './NewPost'
 import Post from './Post'
 import classes from './PostsList.module.css'
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author='Tiago Gomes' content='The Super Book' />
-      <Post author='Semog Ogait' content='The Lamest Book' />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author='Tiago Gomes' content='The Super Book' />
+        <Post author='Semog Ogait' content='The Lamest Book' />
+      </ul>
+    </>
   )
 }
 
