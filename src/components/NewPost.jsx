76cc0e9e2444d5a04063b96266de4428803151e3
Tiago Gomes <tidgomes@gmail.com>
2023-05-24 11:06:51 -0300

    add an event listener to textarea

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,11 +1,20 @@
 import classes from './NewPost.module.css'
 
 function NewPost() {
+  function changeBodyHandler(event) {
+    console.log(event.target.value)
+  }
+
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor='body'>Text</label>
-        <textarea id='body' rows={3} required></textarea>
+        <textarea
+          id='body'
+          rows={3}
+          required
+          onChange={changeBodyHandler}
+        ></textarea>
       </p>
       <p>
         <label htmlFor='name'>Your Name</label>
