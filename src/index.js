b7346aa75b3481871e0532e352178382e725f257
Tiago Gomes <tidgomes@gmail.com>
2023-05-26 11:02:33 -0300

    create a RootLayout to wrap the routes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
+import RootLayout from './routes/RootLayout'
+import NewPost from './components/NewPost'
 
-const router = createBrowserRouter([{ path: '/', element: <App /> }])
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <App /> },
+      { path: '/create-post', element: <NewPost /> }
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
