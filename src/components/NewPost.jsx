69f3a219e612b9fee1bb9274bbc771a8e34a6572
Tiago Gomes <tidgomes@gmail.com>
2023-05-26 08:25:45 -0300

    make NewPost a controlled component by transfering the state from PostList to it

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,20 +1,41 @@
+import { useState } from 'react'
 import classes from './NewPost.module.css'
 
-function NewPost({ onChangeBody, onChangeAuthor, onCancel }) {
+function NewPost({ onCancel }) {
+  const [body, setBody] = useState('')
+  const [author, setAuthor] = useState('')
+
+  function bodyChangeHandler(event) {
+    setBody(event.target.value)
+  }
+  function authorChangeHandler(event) {
+    setAuthor(event.target.value)
+  }
+
+  function submitHandler(event) {
+    event.preventDefault()
+    const postData = {
+      body,
+      author
+    }
+    console.log(postData)
+    onCancel()
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor='body'>Text</label>
         <textarea
           id='body'
           rows={3}
           required
-          onChange={onChangeBody}
+          onChange={bodyChangeHandler}
         ></textarea>
       </p>
       <p>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' required onChange={onChangeAuthor} />
+        <input type='text' id='name' onChange={authorChangeHandler} required />
       </p>
       <p className={classes.actions}>
         <button type='button' onClick={onCancel}>
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -5,29 +5,14 @@ import Modal from './Modal'
 import classes from './PostsList.module.css'
 
 function PostsList({ isPosting, onHideModal }) {
-  const [body, setBody] = useState('')
-  const [author, setAuthor] = useState('')
-
-  function bodyChangeHandler(event) {
-    setBody(event.target.value)
-  }
-  function authorChangeHandler(event) {
-    setAuthor(event.target.value)
-  }
-
   return (
     <>
       {isPosting && (
         <Modal onClose={onHideModal}>
-          <NewPost
-            onChangeBody={bodyChangeHandler}
-            onChangeAuthor={authorChangeHandler}
-            onCancel={onHideModal}
-          />
+          <NewPost onCancel={onHideModal} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={author} body={body} />
         <Post author='Semog Ogait' body='The Lamest Book' />
       </ul>
     </>
